17d575f5200c917e7615ae643cbd8731346b7a79
instrutorvilsonmoro <instrutorvilsonmoro@gmail.com>
2022-11-07 08:57:39 -0300

    Adicionado manipulador de exceção

---
+++ src/main/java/com/aulas/RESTAPI/controllers/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.aulas.RESTAPI.controllers.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.persistence.EntityNotFoundException;
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<StandardError> entidadeNaoEncontrada(EntityNotFoundException e,
+                                                               HttpServletRequest req){
+       StandardError err = new StandardError();
+       err.setTimeStamp(Instant.now());
+       err.setStatus(HttpStatus.NOT_FOUND.value());
+       err.setError("Recurso não encontrado");
+       err.setMessage(e.getMessage());
+       err.setPath(req.getRequestURI());
+       return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+}
