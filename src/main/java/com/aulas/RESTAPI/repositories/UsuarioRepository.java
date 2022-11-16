b08b67423bf736ff32490fd8742e64e5177506c1
instrutorvilsonmoro <instrutorvilsonmoro@gmail.com>
2022-11-16 11:41:07 -0300

    Adicionado método customizado de consulta por email

--- src/main/java/com/aulas/RESTAPI/repositories/UsuarioRepository.java
+++ src/main/java/com/aulas/RESTAPI/repositories/UsuarioRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Usuario findByEmail(String username);
 }
