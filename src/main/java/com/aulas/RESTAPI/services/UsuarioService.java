52df896074e54b781544e14650c3592de98b6a12
instrutorvilsonmoro <instrutorvilsonmoro@gmail.com>
2022-11-16 11:09:01 -0300

    Implementado userDetailsService

--- src/main/java/com/aulas/RESTAPI/services/UsuarioService.java
+++ src/main/java/com/aulas/RESTAPI/services/UsuarioService.java
@@ -3,21 +3,34 @@ package com.aulas.RESTAPI.services;
 import com.aulas.RESTAPI.entidades.Usuario;
 import com.aulas.RESTAPI.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class UsuarioService {
+public class UsuarioService implements UserDetailsService {
     @Autowired
     UsuarioRepository repository;
 
+    @Autowired
+    BCryptPasswordEncoder passwordEncoder;
+
     public Usuario salvar(Usuario usuario){
-       return repository.save(usuario);
+        usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
+        return repository.save(usuario);
     }
 
     public List<Usuario> consultar(){
         return repository.findAll();
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario user = repository.findByEmail(username);
+        return user;
+    }
 }
