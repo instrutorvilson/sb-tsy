57b62ba2c8c6cfc6f0f6f17005c3bffebe7453be
instrutorvilsonmoro <instrutorvilsonmoro@gmail.com>
2022-11-17 09:31:30 -0300

    Alterar aplicação para usar servidor wilfly

--- src/main/java/com/aulas/RESTAPI/RestapiApplication.java
+++ src/main/java/com/aulas/RESTAPI/RestapiApplication.java
@@ -2,9 +2,12 @@ package com.aulas.RESTAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class RestapiApplication {
+@ComponentScan
+public class RestapiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestapiApplication.class, args);
